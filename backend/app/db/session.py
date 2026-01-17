58fa5eefd4e226588315eccbb8a6d2196d7af2ba
ccrriss <zj7166@mun.ca>
2026-01-17 14:52:07 +0800

    change ssl for Render and add .python-version file

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,16 +3,16 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-# connect_args = {}
-# if settings.DATABASE_URL.startswith("postgresql"):
-#     connect_args = {"ssl": ssl.create_default_context()}
+connect_args = {}
+if settings.DATABASE_URL.startswith("postgresql+asyncpg://"):
+    connect_args = {"ssl": "require"}  # Example for setting SSL mode
 
 engine = create_async_engine(
     settings.DATABASE_URL,
     future = True, 
     echo = False,
-    # connect_args = connect_args,
-    connect_args={"ssl": True} if "supabase" in settings.DATABASE_URL else {}, #asyncpg with ssl
+    connect_args = connect_args,
+    # connect_args={"ssl": True} if "supabase" in settings.DATABASE_URL else {}, #asyncpg with ssl
 )
 
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
